49a0296468ad48008a1ac55e6e3ed3d8eb3b0555
alex <shizofranky@gmail.com>
2025-10-08 00:25:40 +0300

    update

---
+++ views/templates/base.template.view.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper--default">
+        <?php
+
+        require './../pages/home.page.view.php';
+
+        ?>
+    </div>
+</body>
+
+</html>
